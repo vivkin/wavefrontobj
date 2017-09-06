a7a9345139e7c9dec7226c8b2e88e4af408c5c3e
Ivan Vashchaev <i_vashchaev@1cgs.net>
2017-09-06 18:27:28 +0300

    added struct for coord, face index, and group store group name instead of fnv1a hash of name extend api with free, load and parse functions

--- main.c
+++ main.c
@@ -6,29 +6,30 @@
 #define dump(f, v) fprintf(stderr, #v " = %" #f "\n", v)
 
 int main(int argc, char **argv) {
-    struct wavefrontobj foo;
+    struct wavefront_mesh mesh;
 
-    dump(zu, sizeof(foo.facegroups));
-    dump(zu, sizeof(foo.facegroups[0]));
-    dump(zu, sizeof(foo.indices));
-    dump(zu, sizeof(foo.indices[0]));
-    dump(zu, sizeof(foo.vertices));
-    dump(zu, sizeof(foo.vertices[0]));
+    dump(zu, sizeof(mesh.groups));
+    dump(zu, sizeof(mesh.groups[0]));
+    dump(zu, sizeof(mesh.indices));
+    dump(zu, sizeof(mesh.indices[0]));
+    dump(zu, sizeof(mesh.vertices));
+    dump(zu, sizeof(mesh.vertices[0]));
 
     if (argc < 2) {
         fprintf(stderr, "usage: %s [file ...]\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    printf("%10s %10s %10s %10s %10s\n", "facegroups", "indices", "vertices", "texcoords", "normals");
+    printf("%10s %10s %10s %10s %10s\n", "groups", "indices", "vertices", "texcoords", "normals");
     for (int argi = 1; argi < argc; ++argi) {
         const char *filename = argv[argi];
         FILE *fp = fopen(filename, "rb");
         if (fp) {
-            struct wavefrontobj mesh;
+            struct wavefront_mesh mesh;
             memset(&mesh, 0, sizeof(mesh));
-            wavefrontobj_parse(&mesh, fp);
-            printf("%10u %10u %10u %10u %10u %s\n", mesh.num_facegroups, mesh.num_indices, mesh.num_vertices, mesh.num_texcoords, mesh.num_normals, filename);
+            wavefrontobj_load(&mesh, fp);
+            printf("%10u %10u %10u %10u %10u %s\n", mesh.num_groups, mesh.num_indices, mesh.num_vertices, mesh.num_texcoords, mesh.num_normals, filename);
+            wavefrontobj_free(&mesh);
             fclose(fp);
         }
     }
--- wavefrontobj.h
+++ wavefrontobj.h
@@ -3,13 +3,31 @@
 
 #include <stdio.h>
 
-struct wavefrontobj {
-    float (*normals)[3], (*texcoords)[3], (*vertices)[3];
-    unsigned int (*facegroups)[2], (*indices)[3];
-    unsigned int num_facegroups, num_indices, num_vertices, num_texcoords, num_normals;
+struct wavefront_coord {
+    float x, y, z;
 };
 
-void wavefrontobj_parse(struct wavefrontobj *mesh, FILE *stream);
+struct wavefront_index {
+    unsigned int vertex, normal, texcoord;
+};
+
+struct wavefront_group {
+    unsigned int first, count;
+    char name[64];
+};
+
+struct wavefront_mesh {
+    struct wavefront_coord *vertices, *normals, *texcoords;
+    struct wavefront_index *indices;
+    struct wavefront_group *groups;
+    unsigned int num_groups, num_indices, num_vertices, num_normals, num_texcoords;
+};
+
+void wavefrontobj_fixup(struct wavefront_mesh *mesh);
+void wavefrontobj_free(struct wavefront_mesh *mesh);
+void wavefrontobj_line(struct wavefront_mesh *mesh, const char *line);
+void wavefrontobj_load(struct wavefront_mesh *mesh, FILE *stream);
+void wavefrontobj_parse(struct wavefront_mesh *mesh, const char *str);
 
 #endif // WAVEFRONTOBJ_H
 
@@ -18,55 +36,86 @@ void wavefrontobj_parse(struct wavefrontobj *mesh, FILE *stream);
 #include <stdlib.h>
 #include <string.h>
 
-#define push_back(data, size, value)                        \
-    do {                                                    \
-        data = realloc(data, sizeof(data[0]) * (size + 1)); \
-        memcpy(data + size++, value, sizeof(data[0]));      \
+#define push_back(data, size, value)                                     \
+    do {                                                                 \
+        *((void **)&data) = realloc(data, sizeof(data[0]) * (size + 1)); \
+        memcpy(data + size++, value, sizeof(data[0]));                   \
     } while (0)
 
-void wavefrontobj_parse(struct wavefrontobj *mesh, FILE *stream) {
-    char line[256];
-    while (fgets(line, sizeof(line), stream)) {
-        if (line[0] == 'v') {
-            float v[3] = {0, 0, 0};
-            sscanf(line + 2, "%f %f %f", v + 0, v + 1, v + 2);
-            if (line[1] == 'n')
-                push_back(mesh->normals, mesh->num_normals, &v);
-            else if (line[1] == 't')
-                push_back(mesh->texcoords, mesh->num_texcoords, &v);
-            else
-                push_back(mesh->vertices, mesh->num_vertices, &v);
-        } else if (line[0] == 'f') {
-            unsigned int count = 0;
-            char *saveptr, *token = strtok_r(line + 2, " ", &saveptr);
-            while (token) {
-                int v[3] = {1, 1, 1};
-                if (sscanf(token, "%d/%d/%d", v + 0, v + 1, v + 2) == 3 ||
-                    sscanf(token, "%d/%d", v + 0, v + 1) == 2 ||
-                    sscanf(token, "%d//%d", v + 0, v + 2) == 2 ||
-                    sscanf(token, "%d", v + 0) == 1) {
-                    if (count > 2) {
-                        unsigned int start = mesh->num_indices - (count - 2) * 3;
-                        unsigned int prev = mesh->num_indices - 1;
-                        push_back(mesh->indices, mesh->num_indices, mesh->indices + start);
-                        push_back(mesh->indices, mesh->num_indices, mesh->indices + prev);
-                    }
-                    v[0] = v[1] > 0 ? (unsigned int)v[0] - 1 : v[0] + mesh->num_vertices;
-                    v[1] = v[1] > 0 ? (unsigned int)v[1] - 1 : v[1] + mesh->num_texcoords;
-                    v[2] = v[2] > 0 ? (unsigned int)v[2] - 1 : v[2] + mesh->num_normals;
-                    push_back(mesh->indices, mesh->num_indices, (&(unsigned int [3]){v[0], v[1], v[2]}));
-                    ++count;
-                }
-                token = strtok_r(NULL, " ", &saveptr);
-            }
-        } else if (line[0] == 'u') {
-            unsigned int v[2] = {mesh->num_indices, 0x811C9DC5};
-            for (const char *s = line + 7; *s && *s != '\n'; ++s)
-                v[1] = ((unsigned int)v[1] ^ (unsigned char)*s) * 0x01000193;
-            push_back(mesh->facegroups, mesh->num_facegroups, &v);
-        }
+void wavefrontobj_fixup(struct wavefront_mesh *mesh) {
+    if (mesh->num_groups) {
+        mesh->groups[mesh->num_groups - 1].count = mesh->num_indices - mesh->groups[mesh->num_groups - 1].first;
+    } else if (mesh->num_vertices && mesh->num_indices) {
+        struct wavefront_group group = {0, mesh->num_indices, ""};
+        push_back(mesh->groups, mesh->num_groups, &group);
     }
-    push_back(mesh->facegroups, mesh->num_facegroups, (&(unsigned int [2]){mesh->num_indices, 0}));
+}
+
+void wavefrontobj_free(struct wavefront_mesh *mesh) {
+    free(mesh->vertices);
+    free(mesh->normals);
+    free(mesh->texcoords);
+    free(mesh->indices);
+    free(mesh->groups);
+}
+
+void wavefrontobj_line(struct wavefront_mesh *mesh, const char *line) {
+    if (line[0] == 'v') {
+        struct wavefront_coord coord = {0, 0, 0};
+        sscanf(line + 2, "%f %f %f", &coord.x, &coord.y, &coord.z);
+        if (line[1] == 'n')
+            push_back(mesh->normals, mesh->num_normals, &coord);
+        else if (line[1] == 't')
+            push_back(mesh->texcoords, mesh->num_texcoords, &coord);
+        else
+            push_back(mesh->vertices, mesh->num_vertices, &coord);
+    } else if (line[0] == 'f') {
+        unsigned int count = 0;
+        int v[3] = {0, 0, 0};
+        int n;
+        line += 2;
+        while (sscanf(line, "%d/%d/%d%n", v + 0, v + 1, v + 2, &n) == 3 ||
+               sscanf(line, "%d/%d%n", v + 0, v + 1, &n) == 2 ||
+               sscanf(line, "%d//%d%n", v + 0, v + 2, &n) == 2 ||
+               sscanf(line, "%d%n", v + 0, &n) == 1) {
+            if (count > 2) {
+                unsigned int start = mesh->num_indices - (count - 2) * 3;
+                unsigned int prev = mesh->num_indices - 1;
+                push_back(mesh->indices, mesh->num_indices, mesh->indices + start);
+                push_back(mesh->indices, mesh->num_indices, mesh->indices + prev);
+            }
+            struct wavefront_index index = {
+                v[0] > 0 ? (unsigned int)v[0] - 1 : v[0] + mesh->num_vertices,
+                v[1] > 0 ? (unsigned int)v[1] - 1 : v[1] + mesh->num_texcoords,
+                v[2] > 0 ? (unsigned int)v[2] - 1 : v[2] + mesh->num_normals};
+            push_back(mesh->indices, mesh->num_indices, &index);
+            count++;
+            v[0] = v[1] = v[2] = 0;
+            line += n;
+        }
+    } else if (line[0] == 'u' || line[0] == 'o' || line[0] == 'g') {
+        if (mesh->num_groups)
+            mesh->groups[mesh->num_groups - 1].count = mesh->num_indices - mesh->groups[mesh->num_groups - 1].first;
+        struct wavefront_group group = {mesh->num_indices, 0, ""};
+        sscanf(line, "%*s %63s", group.name);
+        push_back(mesh->groups, mesh->num_groups, &group);
+    }
+}
+
+void wavefrontobj_load(struct wavefront_mesh *mesh, FILE *stream) {
+    char line[256];
+    while (fgets(line, sizeof(line), stream))
+        wavefrontobj_line(mesh, line);
+    wavefrontobj_fixup(mesh);
+}
+
+void wavefrontobj_parse(struct wavefront_mesh *mesh, const char *str) {
+    while (str) {
+        wavefrontobj_line(mesh, str);
+        if ((str = strchr(str, '\n')))
+            str++;
+    }
+    wavefrontobj_fixup(mesh);
 }
 
 #undef push_back
