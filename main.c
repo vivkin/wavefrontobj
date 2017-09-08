9e374b634fd6bf6228668877b241f2b272055c69
Ivan Vashchaev <i_vashchaev@1cgs.net>
2017-09-08 18:05:47 +0300

    dump function for debug

--- main.c
+++ main.c
@@ -5,6 +5,23 @@
 
 #define dump(f, v) fprintf(stderr, #v " = %" #f "\n", v)
 
+void wavefrontobj_dump(FILE *stream, const struct wavefront_mesh *mesh) {
+    for (size_t i = 0; i < mesh->num_vertices; ++i)
+        fprintf(stream, "v %f %f %f\n", mesh->vertices[i].x, mesh->vertices[i].y, mesh->vertices[i].z);
+    for (size_t i = 0; i < mesh->num_texcoords; ++i)
+        fprintf(stream, "vt %f %f %f\n", mesh->texcoords[i].x, mesh->texcoords[i].y, mesh->texcoords[i].z);
+    for (size_t i = 0; i < mesh->num_normals; ++i)
+        fprintf(stream, "vn %f %f %f\n", mesh->normals[i].x, mesh->normals[i].y, mesh->normals[i].z);
+    for (size_t i = 0; i < mesh->num_groups; ++i) {
+        fprintf(stream, "o %s\n", mesh->groups[i].name);
+        for (size_t j = mesh->groups[i].first; j < mesh->groups[i].first + mesh->groups[i].count; ++j)
+            fprintf(stream, "f %u/%u/%u %u/%u/%u %u/%u/%u\n",
+                    mesh->indices[j + 0].vertex + 1, mesh->indices[j + 0].texcoord + 1, mesh->indices[j + 0].normal + 1,
+                    mesh->indices[j + 1].vertex + 1, mesh->indices[j + 1].texcoord + 1, mesh->indices[j + 1].normal + 1,
+                    mesh->indices[j + 2].vertex + 1, mesh->indices[j + 2].texcoord + 1, mesh->indices[j + 2].normal + 1);
+    }
+}
+
 int main(int argc, char **argv) {
     struct wavefront_mesh mesh;
 
